83e0069f1350cd3b48ebe4d9f61472fec0638e18
Yigal Irani <yigal@symbolclick.com>
2021-09-07 09:46:35 -0500

    Try using keys and VFlags.ONLY_KEYED_CHILDREN or deltas to improve performance

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-import { m, createElement, patch } from 'million';
+import { m, createElement, patch,VFlags} from 'million';
 
 // Initialize app
-var oldvnode=m('div', { id: 'app' }, ['Hello World'])
+var oldvnode=m('div', {key:'root'}, ['Hello World'],VFlags.ONLY_KEYED_CHILDREN)
 const app_elemet = createElement(oldvnode);
 function onclick(){
   gcount+=100
 }
 var offset
-function app(count,depth,id){
+function app(count,depth,key){
   //return 'hello world '+count
   var children=['Hello World '+offset +' '+(count+offset)]
   offset+=1
   
   if (depth>0)
     children=children.concat([1,2].map(x=>app(count,depth-1,x)))
-  var ans=m('div', { id: 'app'+id,onclick }, children)
+  var ans=m('div', {key,onclick}, children,VFlags.ONLY_KEYED_CHILDREN)
   return ans
 }
 document.body.appendChild(app_elemet);
